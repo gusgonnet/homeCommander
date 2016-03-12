32581f7c5229e3f9fd163b89c5c3e4071e3df28f
ceajog <gustavo.gonnet@ericsson.com>
2016-03-12 08:40:52 -0500

    version 0.52: fixing dryer on/off detection

--- homeCommander.ino
+++ homeCommander.ino
@@ -21,14 +21,17 @@
 #include "PietteTech_DHT.h"
 
 #define APP_NAME "Home Commander"
-String VERSION = "Version 0.51";
+String VERSION = "Version 0.52";
 /*******************************************************************************
  * changes in version 0.51:
        * added dryer notifications project with DHT22
+ * changes in version 0.52:
+       * fixed dryer code
 *******************************************************************************/
 
 #define PUSHBULLET_NOTIF "pushbulletGUST"
 #define PUSHBULLET_NOTIF_HOME "pushbulletHOME"
+#define AWS_EMAIL "awsEmail"
 
 /*******************************************************************************
  DHT sensor
@@ -449,81 +452,13 @@ int flood_notify_user()
    return 0;
 }
 
-void dryer_status_old() {
 
-  // Check if we need to start the next sample
-  if (millis() > DHTnextSampleTime) {
-
-    Particle.publish(APP_NAME, "starting DHT22 sample", 60, PRIVATE);
-
-    if (!bDHTstarted) {		// start the sample
-      DHT.acquireAndWait(5);
-      bDHTstarted = true;
-    }
-
-    if (!DHT.acquiring()) {		// has sample completed?
-
-      Particle.publish(APP_NAME, "sample acquired", 60, PRIVATE);
-
-      float temp = (float)DHT.getCelsius();
-      int temp1 = (temp - (int)temp) * 100;
-
-      char tempInChar[32];
-      sprintf(tempInChar,"%0d.%d", (int)temp, temp1);
-
-      //google docs will get this variable - if you are storing the value in a google sheet
-      sprintf(resultstr, "{\"t\":%s}", tempInChar);
-
-      char humiInChar[32];
-      float humid = (float)DHT.getHumidity();
-      int humid1 = (humid - (int)humid) * 100;
-
-      sprintf(humiInChar,"%0d.%d", (int)humid, humid1);
-
-      //google docs will get this variable - if you are storing the value in a google sheet
-      sprintf(humiditystr, "{\"t\":%s}", humiInChar);
-
-      n++;  // increment counter
-      bDHTstarted = false;  // reset the sample flag so we can take another
-      DHTnextSampleTime = millis() + DHT_SAMPLE_INTERVAL;  // set the time for next sample
-
-      //if humidity goes above 50% then we believe the dryer has just started a cycle
-      if ( ( not dryer_on ) and ( humid > 50 ) and ( temp > 30 ) ) {
-        dryer_on = true;
-        humidity_samples_below_10 = 0;
-        Particle.publish(PUSHBULLET_NOTIF_HOME, "Starting drying cycle", 60, PRIVATE);
-        //Particle.publish(AWS_EMAIL, "Starting drying cycle", 60, PRIVATE);
-      }
-
-      //if humidity goes below 10% and temperature goes over 50 degrees for a number of samples
-      // we believe the clothes are dry
-      // modify these parameters if you want to dry even more your clothes
-      // example: to have clothes drier modify to "if ( dryer_on and ( humid < 8) and ( temp > 50 ) ) {"
-      if ( dryer_on and ( humid < 10) and ( temp > 50 ) ) {
-        humidity_samples_below_10 = humidity_samples_below_10 +1;
-      }
-
-      //if there are 5 samples below 10% then we are sure it's almost done
-      if ( dryer_on and (humidity_samples_below_10 >= 5) ) {
-        Particle.publish(PUSHBULLET_NOTIF_HOME, "Your clothes are dry", 60, PRIVATE);
-        //Particle.publish(AWS_EMAIL, "Your clothes are dry", 60, PRIVATE);
-        dryer_on = false;
-      }
-
-      if( dryer_on ) {
-        dryer_stat = "dryer_on";
-      } else {
-        dryer_stat = "dryer_off";
-      }
-
-    }
-
-  }
-
-}
-
-//call this function when the algorithm gets stuck (For instance, when you manually stopped the dryer cycle)
-//you could call this function with the DO Button, the blynk app, the Porter app or even Particle dev
+/*******************************************************************************
+ * Function Name  : resetDryer
+ * Description    : call this function when the algorithm gets stuck (For instance, when you manually stopped the dryer cycle)
+                    you could call this function with the DO Button, the blynk app, the Porter app or even Particle dev
+ * Return         : 0
+ *******************************************************************************/
 int resetDryer(String args) {
   dryer_on = false;
   return 0;
@@ -532,7 +467,6 @@ int resetDryer(String args) {
 /*******************************************************************************
  * Function Name  : dryer_status
  * Description    : reads the temperature of the DHT22 sensor at every DHT_SAMPLE_INTERVAL
-                    if testing the app, it returns right away
  * Return         : 0
  *******************************************************************************/
 int dryer_status() {
@@ -541,8 +475,6 @@ int dryer_status() {
   if (dhtSampleInterval < DHT_SAMPLE_INTERVAL) {
    return 0;
   }
-
-Particle.publish(APP_NAME, "starting DHT22 sample", 60, PRIVATE);
 
   //time is up, reset timer
   dhtSampleInterval = 0;
@@ -564,18 +496,45 @@ Particle.publish(APP_NAME, "starting DHT22 sample", 60, PRIVATE);
   //reset the sample flag so we can take another
   bDHTstarted = false;
 
+  //if humidity goes above 50% then we believe the dryer has just started a cycle
+  if ( ( not dryer_on ) and ( currentHumidity > 50 ) and ( currentTemp > 30 ) ) {
+    dryer_on = true;
+    humidity_samples_below_10 = 0;
+    Particle.publish(PUSHBULLET_NOTIF_HOME, "Starting drying cycle", 60, PRIVATE);
+    Particle.publish(AWS_EMAIL, "Starting drying cycle", 60, PRIVATE);
+  }
+
+  //if humidity goes below 10% and temperature goes over 50 degrees for a number of samples
+  // we believe the clothes are dry
+  // modify these parameters if you want to dry even more your clothes
+  // example: to have clothes drier modify to "if ( dryer_on and ( currentHumidity < 8) and ( currentTemp > 50 ) ) {"
+  if ( dryer_on and ( currentHumidity < 10) and ( currentTemp > 50 ) ) {
+    humidity_samples_below_10 = humidity_samples_below_10 +1;
+  }
+
+  //if there are 5 samples below 10% then we are sure it's almost done
+  if ( dryer_on and (humidity_samples_below_10 >= 5) ) {
+    Particle.publish(PUSHBULLET_NOTIF_HOME, "Your clothes are dry", 60, PRIVATE);
+    Particle.publish(AWS_EMAIL, "Your clothes are dry", 60, PRIVATE);
+    dryer_on = false;
+  }
+
+  if( dryer_on ) {
+    dryer_stat = "dryer_on";
+  } else {
+    dryer_stat = "dryer_off";
+  }
+
   return 0;
 }
 
 /*******************************************************************************
  * Function Name  : publishTemperature
- * Description    : the temperature/humidity passed as parameters get stored in internal variables
-                    and then published
+ * Description    : the temperature/humidity of the dryer are passed as parameters,
+                    get stored in internal variables and then published
  * Return         : 0
  *******************************************************************************/
 int publishTemperature( float temperature, float humidity ) {
-
-Particle.publish(APP_NAME, "starting publish temp", 60, PRIVATE);
 
  char currentTempChar[32];
  currentTemp = temperature;
@@ -592,8 +551,8 @@ Particle.publish(APP_NAME, "starting publish temp", 60, PRIVATE);
  currentHumidityString = String(currentHumidityChar);
 
  //publish readings
- Particle.publish(APP_NAME, "Current temperature: " + currentTempString, 60, PRIVATE);
- Particle.publish(APP_NAME, "Current humidity: " + currentHumidityString, 60, PRIVATE);
+ Particle.publish(APP_NAME, "Dryer temperature: " + currentTempString, 60, PRIVATE);
+ Particle.publish(APP_NAME, "Dryer humidity: " + currentHumidityString, 60, PRIVATE);
 
  return 0;
 
